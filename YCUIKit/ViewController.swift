f7cca5fdd1852585c72210f7f4cad95d987411cd
XiaoChenYung <18730222889@qq.com>
2016-08-18 11:17:29 +0800

    init

---
+++ YCUIKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YCUIKit
+//
+//  Created by tm on 16/8/18.
+//  Copyright © 2016年 tm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ YCUIKit/YCUIKit/YCButton.swift
@@ -0,0 +1,48 @@
+//
+//  YCButton.swift
+//  YCUIKit
+//
+//  Created by tm on 16/8/18.
+//  Copyright © 2016年 tm. All rights reserved.
+//
+
+import UIKit
+
+class YCButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.CGColor
+        }
+    }
+    
+    init(frame: CGRect,isAnimation: Bool) {
+        super.init(frame: frame)
+        if isAnimation {
+            addKeyAnimation()
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func addKeyAnimation() {
+        let layerAn = CAKeyframeAnimation(keyPath: "transform.scale")
+        layerAn.values = [1,0.9,0.8,0.9,1,1.1,1.2,1.1,1]
+        layerAn.duration = 5
+        layer.addAnimation(layerAn, forKey: nil)
+    }
+
+}
